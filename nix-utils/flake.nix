27726d1c3a69cd172dbc5b6b79bfb360f663856d
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-05-12 18:18:39 +0100

    Add a devshell

---
+++ nix-utils/flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/096478927c36";
+    flake-utils.url = "github:numtide/flake-utils";
+    nvtop-nixpkgs.url = "nixpkgs/807c549feabc";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nvtop-nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        nvtop-pkgs = import nvtop-nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              ruff
+              mdformat
+            ];
+          };
+        }
+    );
+}
